7f66f0f98be573e9cad9f708d4ca2b0ee77b2f42
Kamaljeet Rathi <kamaljeet.rathi@infiverve.com>
2015-07-28 15:54:58 +0530

    added timeout field to scripts

--- cloud/operation/list_instance.rb
+++ cloud/operation/list_instance.rb
@@ -1,18 +1,39 @@
 #begin
-@log.trace("Started executing 'flint-util:jcloud:operation:list_instance.rb' flintbit...")
+@log.trace("Started executing 'flint-util:cloud:operation:list_instance.rb' flintbit...")
 #Flintbit Input Parameters
-#Mandatory
-@connector_name= @input.get("connector_name")                 #Name of the Cloud Connector
-@action = @input.get("action")                                #Action
+#Mandatory  
+@connector_name= @input.get("connector_name")               #Name of the Cloud Connector
+@action = @input.get("action")                              #Action
+#optional
+@key = @input.get("key")                                    #Key
+@secret = @input.get("secret")                              #Secret
+@cloud = @input.get("cloud")                                #Cloud
+@request_timeout= @input.get("timeout")                     #timeout
+
 
 @log.info("Flintbit input parameters are, connector name :: #{@connector_name} |
-	                                       action ::        #{@action}")
+	                                       action ::        #{@action}|
+                                           id ::            #{@id}|
+                                           key ::           #{@key}|
+                                           secret ::        #{@secret}|
+                                           cloud ::         #{@cloud}|
+                                           timeout ::       #{@request_timeout}")
 
-@log.trace("Calling #{@connector_name}...")
 
- response = @call.connector(@connector_name)
-                 .set("action",@action)
-                 .sync
+
+ connector_call = @call.connector(@connector_name)
+                  .set("action",@action)
+                  .set("key",@key)
+                  .set("cloud",@cloud)
+                  .set("secret",@secret)
+
+ if @request_timeout.nil? || @request_timeout.is_a?(String)
+   @log.trace("Calling #{@connector_name} with default timeout...")
+	 response = connector_call.sync
+else
+   @log.trace("Calling #{@connector_name} with given timeout #{request_timeout.to_s}...")
+	 response = connector_call.timeout(@request_timeout).sync
+end
 
 #Cloud Connector Response Meta Parameters
 response_exitcode=response.exitcode           #Exit status code
@@ -24,7 +45,7 @@ result = response.get("list-instance")        #Response body
 
 if response.exitcode == 0
     result.each do |instance_id|
-	@log.info("Amazon EC2 Instance  id : #{instance_id.get("id")} |
+	@log.info("#{@connector_name} Instance  id : #{instance_id.get("id")} |
 						  Provider id :  #{instance_id.get("provider-id")}
 					         Location :  #{instance_id.get("location")}")
 end
--- cloud/operation/stop_instance.rb
+++ cloud/operation/stop_instance.rb
@@ -1,27 +1,47 @@
 #begin
-@log.trace("Started executing 'flint-util:jcloud:operation:stop_instance.rb' flintbit...")
+@log.trace("Started executing 'flint-util:cloud:operation:stop_instance.rb' flintbit...")
 #Flintbit Input Parameters
-#Mandatory  
+#Mandatory 
 @connector_name= @input.get("connector_name")               #Name of the Cloud Connector
 @action = @input.get("action")                              #Action
 @id = @input.get("id")                                      #Id
+#optional
+@key = @input.get("key")                                    #Key
+@secret = @input.get("secret")                              #Secret
+@cloud = @input.get("cloud")                                #Cloud
+@request_timeout= @input.get("timeout")                     #timeout
+
 @log.info("Flintbit input parameters are, connector name :: #{@connector_name} |
-	                                       action ::        #{@action}
-                                           id ::            #{@id}")
+	                                       action ::        #{@action}|
+                                           id ::            #{@id}|
+                                           key ::           #{@key}|
+                                           secret ::        #{@secret}|
+                                           cloud ::         #{@cloud}|
+                                           timeout ::       #{@request_timeout}")
 
-@log.trace("Calling #{@connector_name}...")
-
- response = @call.connector(@connector_name)
+connector_call = @call.connector(@connector_name)
                   .set("action",@action)
                   .set("id",@id)
-                  .sync
+                  .set("key",@key)
+                  .set("cloud",@cloud)
+                  .set("secret",@secret)
+                  
+
+if @request_timeout.nil? || @request_timeout.is_a?(String)
+   @log.trace("Calling #{@connector_name} with default timeout...")
+	 response = connector_call.sync
+else
+   @log.trace("Calling #{@connector_name} with given timeout #{request_timeout.to_s}...")
+	 response = connector_call.timeout(@request_timeout).sync
+end
+
 #Cloud Connector Response Meta Parameters
 response_exitcode=response.exitcode           #Exit status code
 response_message=response.message             #Execution status message
 
 #Cloud Connector Response Parameters
 result = response.get("message")              #Response body
-             
+
 if response.exitcode == 0  
 	@log.info("SUCCESS in executing cloud Connector where, exitcode :: #{response_exitcode} | 
     	                                                   message ::  #{response_message}")
