63289b8364052e84bd20019d849f9c1cb7513c15
Kamaljeet Rathi <kamaljeet.rathi@infiverve.com>
2015-07-29 19:13:00 +0530

    added create_instance flintbit

--- cloud/operation/create_instance.rb
+++ cloud/operation/create_instance.rb
@@ -1,36 +1,46 @@
 #begin
-@log.trace("Started executing 'flint-util:cloud:operation:describe_instance.rb' flintbit...")
+@log.trace("Started executing 'flint-util:cloud:operation:create_instance.rb' flintbit...")
 #Flintbit Input Parameters
 #Mandatory  
 @connector_name= @input.get("connector_name")               #Name of the Cloud Connector
 @action = @input.get("action")                              #Action
-@id = @input.get("id")                                      #Id of server
+@locationId = @input.get("location-id")
+@imageId = @input.get("image-id")
+@hardwareId = @input.get("hardware-id")
 #optional
 @key = @input.get("key")                                    #Key
 @secret = @input.get("secret")                              #Secret
 @cloud = @input.get("cloud")                                #Cloud
 @request_timeout= @input.get("timeout")                     #timeout
 
+
 @log.info("Flintbit input parameters are, connector name :: #{@connector_name} |
 	                                       action ::        #{@action}|
-                                           id ::            #{@id}
-                                           key ::           #{@key}
-                                           secret ::        #{@secret}
-                                           cloud ::         #{@cloud}
-                                           timeout ::       #{@request_timeout}")
+                                           id ::            #{@id}|
+                                           key ::           #{@key}|
+                                           secret ::        #{@secret}|
+                                           cloud ::         #{@cloud}|
+                                           timeout ::       #{@request_timeout}|
+                                           location ::      #{@locationId}|
+                                           imageid ::       #{@imageId}|
+                                           hardware::       #{@hardwareId}")
 
-connector_call = @call.connector(@connector_name)
+
+
+ connector_call = @call.connector(@connector_name)
                   .set("action",@action)
-                  .set("id",@id)
                   .set("key",@key)
                   .set("cloud",@cloud)
                   .set("secret",@secret)
+                  .set("location-id",@locationId)
+                  .set("image-id",@imageId)
+                  .set("hardware-id",@hardwareId)
 
-if @request_timeout.nil? || @request_timeout.is_a?(String)
+ if @request_timeout.nil? || @request_timeout.is_a?(String)
    @log.trace("Calling #{@connector_name} with default timeout...")
 	 response = connector_call.sync
 else
-   @log.trace("Calling #{@connector_name} with given timeout #{request_timeout.to_s}...")
+   @log.trace("Calling #{@connector_name} with given timeout #{@request_timeout.to_s}...")
 	 response = connector_call.timeout(@request_timeout).sync
 end
 
@@ -39,22 +49,15 @@ response_exitcode=response.exitcode           #Exit status code
 response_message=response.message             #Execution status message
 
 #Cloud Connector Response Parameters
-result = response.get("describe-instance")    #Response Body
-
+result = response.get("create-instance")        #Response body
+              
 
 if response.exitcode == 0
     result.each do |instance_id|
-	@log.info("#{@connector_name} Instance id :      #{instance_id.get("id")} |
-							   Provider id :         #{instance_id.get("provider-id")}|
-							   Location :            #{instance_id.get("location")}|
-							   imageid :             #{instance_id.get("image-id")}|
-                               os :                  #{instance_id.get("os")}|
-                               backend-status :      #{instance_id.get("backend-status")}|
-                               login-port :          #{instance_id.get("login-port")}|
-                               hostname :            #{instance_id.get("host-name")}|
-                               private-address :     #{instance_id.get("private-address")}|
-                               hardware :            #{instance_id.get("hardware")}")
-end
+	@log.info("#{@connector_name} Instance  id : #{instance_id.get("id")} |
+						  Provider id :  #{instance_id.get("provider-id")}
+					         Location :  #{instance_id.get("location")}")
+	end
 	@log.info("SUCCESS in executing cloud Connector where, exitcode :: #{response_exitcode} | 
     	                                                   message ::  #{response_message}")
 	@log.info("HTTP Response Body :: #{result}")
@@ -65,6 +68,6 @@ else
 	@log.error("ERROR in executing cloud Connector where, exitcode :: #{response_exitcode} | 
 		                                                  message ::  #{response_message}")
     @output.exit(1,response_message)
-    end
-@log.trace("Finished executing 'flint-util:jcloud:operation:describe_instance.rb' flintbit...")
+end
+   @log.trace("FINISHED executing 'flint-util:jcloud:operation:list_instance.rb' flintbit...")
 #end
