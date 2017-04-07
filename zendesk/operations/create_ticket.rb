1d9a43361fd2204efc67bb6bcd4f173d9a711934
anurag-infiverve <anurag.dubey@infiverve.com>
2017-04-07 12:57:00 +0530

    initial commit

---
+++ zendesk/operations/create_ticket.rb
@@ -0,0 +1,74 @@
+# begin
+@log.trace("Started executing 'zendesk:create_ticket.rb' flintbit...")
+begin
+    # Flintbit Input Parameters
+    @connector_name = @input.get('connector_name') # Name of the zendesk Connector
+    #if you not want to provide connector name each time, jsut uncomment the below line and set zendesk connector_name.
+    # @connector_name = 'zendesk' if @connector_name.nil?
+    @action = 'create-ticket' # Contains the name of the operation: create-tickets
+    @data ={}     # Hash to store Key, Value pairs
+    @custom_fields = Array.new    # The Array of custom fields
+    @custom_field = {} # Hash to store Key, Value pairs
+    @comment = {} # Hash to store Key, Value pairs
+    @data['description'] = @input.get('description')
+    @data['priority'] = @input.get('priority')
+    @data['status'] = @input.get('status')
+    @data['recipient'] = @input.get('recipient')
+    @data['assignee_id'] = @input.get('assignee_id')
+    @data['type'] = @input.get('type')
+    @data['subject'] = @input.get('subject')
+    @data['requester_id'] = @input.get('requester_id')
+    @data['problem_id'] = @input.get('problem_id')
+    @data['collaborator_ids'] = @input.get('collaborator_ids')
+    @comment['body'] = @input.get('comment')
+    @data['comment'] = @comment
+    @custom_field['id'] =  @input.get('custom_fields_id')
+    @custom_field['value'] = @input.get('custom_fields_value')
+    @custom_fields = @custom_field
+    @data['custom_fields'] = @custom_fields
+
+    @request_timeout = @input.get('timeout')
+
+    if @connector_name.nil? || @connector_name.empty?
+     raise 'Please provide "Zendesk connector name (connector_name)" '
+    end
+    if @data.nil? || @data.empty?
+     raise 'Please provide data to create ticket'
+    end
+
+    @log.info("Flintbit input parameters are, connector name :: #{@connector_name} |action :: #{@action} | data :: #{@data}")
+
+    connector_call = @call.connector(@connector_name)
+                    .set('action', @action)
+                    .set('data', @data)
+
+
+ if @request_timeout.nil? || @request_timeout.is_a?(String)
+         @log.trace("Calling #{@connector_name} with default timeout...")
+         response = connector_call.sync
+     else
+         @log.trace("Calling #{@connector_name} with given timeout #{@request_timeout}...")
+         response = connector_call.timeout(@request_timeout).sync
+     end
+
+    # zendesk Connector Response Meta Parameters
+    response_exitcode = response.exitcode           # Exit status code
+    response_message = response.message             # Execution status message
+
+    # zendesk Connector Response Parameters
+    response_body = response.get('body') # Response body
+
+    if response_exitcode == 0
+        @log.info("Success in executing zendesk Connector, where exitcode :: #{response_exitcode} | message :: #{response_message}")
+        @log.info("Result : #{response_body}")
+        @output.setraw('result', response_body)
+    else
+        @log.error("Failure in executing zendesk Connector where, exitcode :: #{response_exitcode} | message :: #{response_message} | error :: #{response_body}")
+        @output.set('exitcode', response_exitcode).set('error', response_message)
+    end
+rescue Exception => e
+    @log.error(e.message)
+    @output.set('exit-code', -1).set('message', e.message)
+end
+@log.trace("Finished executing 'zendesk:create_ticket.rb' flintbit...")
+# end
