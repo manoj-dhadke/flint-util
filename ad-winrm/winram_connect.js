2326064b9db5944bb69afb431afd43b2946854b4
atharva <atharva_infiverve>
2018-04-04 19:16:06 +0530

    updated winram_connect.js flintbit

--- ad-winrm/winram_connect.js
+++ ad-winrm/winram_connect.js
@@ -1,7 +1,7 @@
 log.trace("Started executing flint-util:ad-winrm:winrm_commonconnect.js")
 // Input from Flint's global config
 target = config.global("winrm.target")
-log.info("target::" + target)
+log.info("target::>>>>>>>>>" + target)
 username = config.global("winrm.username")
 password = config.global("winrm.password")
 transport = config.global("winrm.transport")
@@ -11,6 +11,7 @@ operation_timeout = 80
 shell = "ps"
 command = input.get("command")
 log.info("Command::" + command)
+ticket_id = input.get("ticket_id") // ServiceAide ticket Id
 
 // Flint connector call
 connector_response = call.connector(connector_name)
@@ -39,5 +40,17 @@ if (response_exitcode == 0) {
     log.error("Failure in executing WinRM Connector where, exitcode ::" + response_exitcode + "| message ::" + response_message)
     output.set("error", response_message).set("exit-code", -1)
     log.trace("Finished executing winrm flintbit with error...")
+    work_description_fail = "Error occured while executing ticket" + " " + ticket_id + ". " + "The error occured due to" + " " + error_message
+    update_serviceaide_status = call.bit("serviceaide:servicerequest:service_request_update_error_status.groovy")
+        .set("ticket_id", ticket_id)
+        .sync()
+    if (update_serviceaide_status.exitcode() == 0) {
+        add_serviceaide_worklog = call.bit("serviceaide:servicerequest:service_request_add_error_worklog.groovy")
+            .set("ticket_id", ticket_id)
+            .set("work_description", work_description_fail)
+            .sync()
+    } else {
+        log.info("Failed to update ticket status")
+    }
 }
 log.trace("Finished executing flint-util:ad-winrm:winrm_commonconnect.js")
