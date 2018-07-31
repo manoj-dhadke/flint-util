9928290e6f23d7b6ced5eadac83c4060df470b70
atharva <atharva_infiverve>
2018-07-31 11:57:21 +0530

    Updated winrm_commonconnect.js and enable ad user flintbits

--- ad-winrm/enable_ad_account.js
+++ ad-winrm/enable_ad_account.js
@@ -1,38 +1,64 @@
-log.trace("Started executing flint-demo-box:ad:enable_account.rb flintbit...")
+/**
+** Creation Date: 20th May 2018
+** Summary: This is Active Directory User Account Enable flintbit.
+** Description: This flintbit is developed to enable Active Directory User Account using WinRM connector.
+**/
 
-    login_name = input.get("login-name")
-    command = "Enable-adAccount"+" "+ login_name
-    //# Call flintbit synchronously and set arguments
-    flintbit_response = call.bit("flint-util:ad-winrm:winram_connect.js")
-                             .set("command", command)
-                             .sync()
+log.trace("Started executing flint-util:ad-winrm:enable_ad_account.js flintbit.")
+try {
+  // Input from Flint
+  login_name = input.get('login_name')
+  command = "Import-module activedirectory;Enable-adAccount" + " " + login_name
+  target = input.get('target')
+  username = input.get('username')
+  password = input.get('password')
 
-success_message = flintbit_response.message
-result= flintbit_response.get("result")
-error_message= flintbit_response.get("error")
-
-if (flintbit_response.exitcode() == 0)
-{
-log.info("Success in executing WinRM Connector, where exitcode ::"+ flintbit_response.exitcode()+" | message ::"+ success_message)
-    log.info("Command executed ::"+command+" | Command execution results ::"+ result)
-    user_message = "**Account with username :'"+login_name+"' is enabled successfully**"
-    log.info("Account Enabled operation successful    ::user_message::"+ user_message)
-    output.set("result", result).set("user_message",user_message)
-    log.trace("Finished executing winrm flintbit with success...")
+  if (login_name == null || login_name == "") {
+    throw "Please provide Login-Name of AD user Account"
   }
-else
-{
-    log.error("Failure in executing WinRM Connector where, exitcode ::"+ flintbit_response.exitcode()+" | message ::"+ error_message)
-    log.error("Account Enabled operation unsuccessful")
-    //#@new_error = @error_message[/[^\d]+/]
-    //#@error_msg = @new_error.gsub(/^At line\:/, '')
-    //#@error_msgs = @error_msg.gsub(/\'/,'')
-    //#@user_message = """**Error:#{@error_msgs}**"""
-  //  a = error_message.split(/At line/).first
-  //  user_message = "**Error:**"+a.rstrip
-    log.error("Failure in executing WinRM Connector where, exitcode ::"+ flintbit_response.exitcode()+" | message ::"+ error_message +" | for user ::"+ user_message)
-    output.set("error", error_message).set("user_message",user_message)
-    log.trace("Finished executing winrm flintbit with error...")
-}
+  if (target == null || target == "") {
+    throw "Please provide 'target/IP' to connect with machine"
+  }
 
-log.trace("Finished executing flint-demo-box:ad:enable_account.rb flintbit")
+  if (username == null || username == "") {
+    throw "Please provide 'username' to connect with machine"
+  }
+
+  if (password == null || password == "") {
+    throw "Please provide 'password' to connect with machine"
+  }
+
+  // Call flintbit synchronously and set required arguments/parameters
+  flintbit_call_response = call.bit('flint-util:ad-winrm:winrm_commonconnect.js')
+                               .set('command', command)
+                               .set("target", target)
+                               .set("username", username)
+                               .set("password", password)
+                               .timeout(300000)
+                               .sync()
+
+ //extracting response of flintbit call                              
+  success_message = flintbit_call_response.message
+  result = flintbit_call_response.get("result")
+  error_message = flintbit_call_response.get("error")
+  exitcode = flintbit_call_response.get("exit-code")
+
+  if (exitcode == 0) {
+    log.info("Success in executing WinRM Connector, where exitcode ::" + flintbit_call_response.exitcode() + "| message ::" + success_message)
+    log.info("Command executed ::" + command + "| Command execution results ::" + result)
+    user_message = "**Account with username :" + login_name + "is enabled successfully**"
+    log.info("Account Enable operation successful::user_message::" + user_message)
+    output.set('result', result).set('user_message', user_message).set('exit-code', 0)
+  }
+  else {
+    log.error("Failure in executing WinRM Connector where, exitcode ::" + flintbit_call_response.exitcode() + " | message ::" + error_message)
+    log.error("Account Enable operation unsuccessful")
+    log.error("Failure in executing WinRM Connector where, exitcode ::" + flintbit_call_response.exitcode() + " | message ::" + error_message + " | for user ::" + user_message)
+    output.set("error", error_message).set("user_message", user_message).set('exit-code', -1)
+  }
+} catch (error) {
+  log.error("Error message: " + error)
+  output.set('exit-code', 1)
+
+}
+log.trace("Finished executing 'example:ad:enable_user.js' flintbit")
---
+++ ad-winrm/winrm_commonconnect.js
@@ -0,0 +1,71 @@
+/**
+** Creation Date: 20th May 2018
+** Summary: This is winrm_commonconnect.js flintbit to execute commands on Windows server remotely.
+** Description: The winrm_commonconnect flintbit is developed as a common flintbit which uses WinRM connector to connect and execute commands on Windows server.
+**/
+
+log.trace("Started executing flint-util:ad-winrm:winrm_commonconnect.js flintbit.")
+// Input from Flint
+try {
+    target = input.get("target")
+    username = input.get("username")
+    password = input.get("password")
+    transport = config.global("winrm.transport")
+    port = config.global("winrm.port")
+    connector_name = config.global("winrm.connector_name")
+    shell = config.global("winrm.shell")
+    no_ssl_peer_verification = config.global("winrm.no_ssl_peer_verification")
+    command = input.get("command")
+    operation_timeout = 80
+
+    if (target == null || target == "") {
+        throw "Please provide 'target/IP' to connect with machine"
+    }
+    if (username == null || username == "") {
+        throw "Please provide 'username' to connect with machine"
+    }
+    if (password == null || password == "") {
+        throw "Please provide 'password' to connect with machine"
+    }
+
+    if (command == null || command == "") {
+        throw "Command not provided"
+    }
+
+    // Flint connector call
+    connector_response = call.connector(connector_name)
+                             .set("target", target)
+                             .set("username", username)
+                             .set("password", password)
+                             .set("shell", shell)
+                             .set("port", port)
+                             .set("transport", transport)
+                             .set("command", command)
+                             .set("operation_timeout", operation_timeout)
+                             .set("no_ssl_peer_verification", no_ssl_peer_verification)
+                             .timeout(300000)
+                             .sync()
+
+
+    //extracting response of connector call
+    response_exitcode = connector_response.exitcode() // exitcode
+    response_message = connector_response.message() // message
+    response_body = connector_response.get("result")
+    //exception handling
+    if (response_exitcode == 0) {
+        log.info("Success in executing WinRM Connector, where exitcode ::" + response_exitcode + "| message ::" + response_message)
+        log.info("Command executed ::" + command + " | Command execution results ::" + response_body)
+        output.set("result", response_body).set("exit-code", 0)
+        log.trace("Finished executing flintbit with success...")
+    } else {
+        log.error("Failure in executing WinRM Connector where, exitcode ::" + response_exitcode + "| message ::" + response_message)
+        output.set("error", response_message).set("exit-code", -1)
+        log.trace("Finished executing winrm flintbit with error...")
+    }
+
+} catch (error) {
+    log.error("Error message: " + error)
+    output.set('exit-code', 1)
+}
+
+log.trace("Finished executing flint-util:ad-winrm:winrm_commonconnect.js flintbit.")
