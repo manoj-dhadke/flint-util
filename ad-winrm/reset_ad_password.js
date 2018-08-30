07bd13987d5afba2fab669e26488f04d886db0ae
manoj <manoj.dhadke@infiverve.com>
2018-08-30 12:33:17 +0530

    Updating reset_ad_password.js flintbit

--- ad-winrm/reset_ad_password.js
+++ ad-winrm/reset_ad_password.js
@@ -1,3 +1,8 @@
+/**
+** Creation Date: 23th August 2018
+** Summary: This is Reset Active Directory User Password flintbit.
+** Description: This flintbit is developed to reset AD user password after receiving service request from Freshserver.
+**/
 log.trace("Started executing flint-util:ad-winrm:reset_password.js flintbit...")
 
 try{
@@ -5,27 +10,22 @@ try{
     login_name = input.get("login_name")
     log.info(" login_name:" + login_name)
 
-    // NEW RESET DEFAULT PASSWORD
+    // New default password
     password = "Welcome@123"
 
     log.trace(input)
 
     // Inputs for winrm 
     target = input.get('target')
-     target_username = input.get('target_username')
-     target_password = input.get('target_password')
-    
-
-    log.info("Before flintbit call")
- 
-    // COMMAND TO RESET PASSWORD
-
+    target_username = input.get('target_username')
+    target_password = input.get('target_password')
+     
+    // Command to reset AD user password
     command = " Import-module activedirectory;Set-ADAccountPassword -Identity " + " " + login_name.toString() + " " + " -Reset -NewPassword (ConvertTo-SecureString -AsPlainText " + " " + password.toString() + " " + " -Force)"
 
     log.info("Command : "+ command)
 
-
-    // CALL TO RESET PASSWORD
+    // Flintbit call to reset password
     flintbit_response = call.bit("flint-util:ad-winrm:winrm_commonconnect.js")
                             .set("command", command)
                             .set("target", target)
@@ -33,28 +33,24 @@ try{
                             .set("password",  target_password)
                             .sync()
 
-    //extracting response of connector call
-     message = flintbit_response.message()
+    // Extracting response of flintbit call
+    message = flintbit_response.message()
     result = flintbit_response.get("result")
 
     first_error_message = flintbit_response.get("error")
     exitcode = flintbit_response.get("exit-code")
 
-    log.trace("First WINRM ==========> "+flintbit_response)
-
-    //exception handling
-    //case to execute if  message is the response
+    // Exit-code Handling
     if (exitcode == 0) {
         
         log.info("Success in executing WinRM Connector! \nExitcode FOR RESET PASSWORD :: " + exitcode + "\nMessage :: " +  message)
-
         log.info("Reset password successful")
-        log.trace("Finished executing winrm flintbit with success...")
+        log.trace("Finished executing winrm flintbit with success.")
 
-        // COMMAND TO CHANGE PASSWORD ON FIRST LOGON
+        // Command to change password on first logon after resetting the password
         command = "Import-module activedirectory;Set-ADUser" + " " +  login_name + " " + " -changepasswordatlogon 1"
 
-        // CALL TO CHANGE PASSWORD ON FIRST LOGON
+        // Flintbit call to change the password on first logon after resetting the password
         flintbit_response_first_logon = call.bit("flint-util:ad-winrm:winrm_commonconnect.js")
                                             .set("command", command)
                                             .set("target", target)
@@ -62,28 +58,20 @@ try{
                                             .set("password",  target_password)
                                             .sync()
 
-
         exitcode_response = flintbit_response_first_logon.get("exit-code")
         second_error_message = flintbit_response_first_logon.get("error")
-        if(exitcode_response == 0){
-
-            log.trace("SECOND WINRM ==========> "+flintbit_response_first_logon)
-
-
+        
+        if(exitcode_response == 0) {
             log.trace("Exit-code for FIRST LOGON: "+exitcode_response)
             log.info("Success in executing command of change password at first login")
-            
-        }
-        else {
+        } else {
             log.error("ERROR AT FIRST LOGON: "+second_error_message)
             log.error("Error in executing command to change password at first login")
         }
-
         output.set("exit-code", exitcode).set('message', message)
     }else{
         output.set("exit-code", exitcode).set('message', first_error_message)
         log.error("ERROR AT RESET PASSWORD: "+first_error_message)
- 
     }
 }catch(error){
     log.trace("Catch Exception: "+error)
