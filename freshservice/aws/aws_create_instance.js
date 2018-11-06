893b8e4b44b1288622a0c01271ea9f35339f5a78
abhinav-infiverve <abhinav.kumar@getflint.io>
2018-11-06 11:27:01 +0530

    Freshservice new flintbits

---
+++ freshservice/aws/aws_create_instance.js
@@ -0,0 +1,151 @@
+/**
+** Creation Date: 23th August 2018
+** Summary: This is Create AWS instance service flintbit.
+** Description: This flintbit is developed to create an AWS instance after receiving request from Freshserver.
+**/
+log.trace("Started executing flint-util:freshservice:wrapper_aws_create_instance.js flintbit.")
+log.info("Input.....:: "+input)
+try{
+    // Get Flint Job ID
+    flint_job_id = input.jobid()
+
+    // Inputs to create AWS instance, set in service config
+    region = input.get('region')
+    key_name = input.get('key_name')
+    subnet_id = input.get('subnet_id')
+    access_key = input.get('access-key')    
+    max_instance = input.get('max_instance')
+    min_instance = input.get('min_instance')
+    security_key = input.get('security-key')
+    aws_connector_name = input.get('aws_connector_name')
+    availability_zone = input.get('availability_zone')
+
+    // Service ID/ Ticket ID
+    ticket_id = input.get('freshdesk_webhook.ticket_id')
+    ticket_id = ticket_id.replace(/^\D+/g, '')
+    log.info(ticket_id)
+
+    // Ticket service item fields to be parsing
+     ticket_service_item_fields = input.get('freshdesk_webhook.ticket_service_item_fields')
+ 
+    // Parse the service item fields by calling flintbit
+    parse_flintbit_call_response = call.bit('flint-util:freshservice:extract_sr_items_fields.groovy')
+                                       .set('ticket_service_item_fields', ticket_service_item_fields)
+                                       .sync()
+    
+    // Get instance size(type) from the response of freshservice extract_sr_items_fields
+    instance_size = parse_flintbit_call_response.get('data').get('Instance Size')
+    // Getting OS
+    os_type = parse_flintbit_call_response.get('data').get('Operating System')
+    log.info("Ticket ID " + ticket_id)
+    log.info("Instance Size " + instance_size)
+    log.info("OS: " + os_type)
+
+    // Inputs for setting service status
+    domain_name = input.get('domain_name')
+    email = input.get('email')
+    password = input.get('password')
+    status = input.get('status')
+    connector_name = input.get('connector_name')
+    ticket_type = input.get('ticket_type')
+
+    // Inputs for creating notes
+    acknowledgement_body = "Flint acknowledged request for AWS instance creation and automation has been initiated for Job ID ("+flint_job_id+")"
+    private_note = input.get('private_note')
+
+    // Getting relevant ami ID
+    ami_id = input.get('os_mapping').get(os_type)
+    log.trace("AMI ID: " + ami_id)
+
+    // Add service acknowledgement note
+    flintbit_call_response = call.bit("flint-util:freshservice:add_note.js")
+                                 .set('domain_name', domain_name)
+                                 .set('email', email)
+                                 .set('password', password)
+                                 .set('private_note',private_note)
+                                 .set('body', acknowledgement_body)
+                                 .set('connector_name',connector_name)
+                                 .set('ticket_id',ticket_id)
+                                 .set('ticket_type',ticket_type)
+                                 .sync()
+
+    log.trace("First Add note call is done")
+
+    first_note_exitcode = flintbit_call_response.get("exit-code")
+    first_note_response_message = flintbit_call_response.get("message")
+    log.trace(first_note_response_message)
+    log.trace(first_note_exitcode)
+    
+    if(first_note_exitcode == 0){
+        log.trace(input)
+
+        create_aws_flintbit_call_response = call.bit("fb-cloud:aws-ec2:operation:create_instance.rb")
+                                                .set('connector_name',aws_connector_name)   
+                                                .set('ami_id', ami_id)                  // Image ID
+                                                .set('instance_type', instance_size)    
+                                                .set('min_instance', min_instance)
+                                                .set('max_instance', max_instance)
+                                                .set('access-key',access_key)
+                                                .set('security-key', security_key)
+                                                .set('availability_zone', availability_zone)
+                                                .sync()
+        
+        // Getting exit-code for create instance flinbit call
+        create_instance_exit_code = create_aws_flintbit_call_response.get("exit-code")
+        create_instance_response_message = create_aws_flintbit_call_response.get("message")
+
+        // Getting instance information
+        instance_info = create_aws_flintbit_call_response.get('instances-info')
+        instance_id = instance_info[0].get('instance-id')
+        log.info("Instance_Info: " +instance_id)
+        // Getting private IP
+        private_ip = instance_info[0].get('private-ip')
+        log.info("Private IP: " + private_ip)
+
+        // Final note body
+        final_body = "Service request is completed by Flint. Marked service request as resolved.\n Instance ID: " +instance_id + "\n Instance Type: " + instance_size + "\nOS: "+ os_type +"\n Private IP: " + private_ip
+
+        user_message = "<b>Flint Automation:</b>  Created AWS instance successfully. <br><b>Instance ID:</b> " +instance_id + " <br><b>Instance Type:</b> " + instance_size + "<br><b>OS:</b> "+ os_type +"<br><b>Private IP:</b> " + private_ip
+
+        if(create_instance_exit_code == 0){
+            // Set service status
+            update_ticket_call_response = call.bit('flint-util:freshservice:update_ticket.js')
+                                              .set('domain_name', domain_name)
+                                              .set('email',email)
+                                              .set('password', password)
+                                              .set('status', status)
+                                              .set('connector_name', connector_name)
+                                              .set('ticket_id', ticket_id)
+                                              .set('ticket_type', ticket_type)
+                                              .sync()
+            // Get Service status response message
+            update_ticket_response_message = update_ticket_call_response.get("message")
+            log.trace(update_ticket_response_message)
+
+            // Add note after instance is created
+           second_flintbit_call_response =  call.bit("flint-util:freshservice:add_note.js")
+                                                .set('domain_name', domain_name)
+                                                .set('email', email)
+                                                .set('password', password)
+                                                .set('private_note',private_note)
+                                                .set('body',final_body)
+                                                .set('connector_name',connector_name)
+                                                .set('ticket_id',ticket_id)
+                                                .set('ticket_type',ticket_type)
+                                                .sync()
+
+            second_note_response_message =  second_flintbit_call_response.get("message")
+            log.trace(second_note_response_message)  
+
+            // Setting user message (will be visible on CMP)
+            output.set('exit-code', 0).set('user_message', user_message)
+        }else{
+            log.error("Unable to create AWS instance : "+ create_instance_response_message)
+        }
+    }else{
+        log.error("Unable to create note : " + first_note_response_message)
+    }
+}catch(error){
+    log.error("Error : " + error)
+}
+log.trace("Finished executing flint-util:freshservice:wrapper_aws_create_instance.js flintbit.")
---
+++ freshservice/router.js
@@ -0,0 +1,168 @@
+log.trace("Started executing freshservice:router:router.js")
+log.info("Input from freshservice.....:: " + input)
+//AWS inputs
+aws_config = input.get("aws_config")
+aws_connector_name = aws_config.get('aws_connector_name')         // Name of the Amazon EC2 Connector
+access_key = aws_config.get('access-key')
+security_key = aws_config.get('security-key')
+aws_region = aws_config.get('region')	                             // Amazon EC2 region (default region is 'us-east-1')
+//Azure inputs
+azure_config = input.get("azure_config")
+azure_connector_name = azure_config.get('azure_connector_name')
+azure_region = azure_config.get('region')
+key = azure_config.get('key')
+tenant_id = azure_config.get('tenant-id')
+subscription_id = azure_config.get('subscription-id')
+client_id = azure_config.get('client-id')
+//GCP Inputs
+gcp_config = input.get("gcp_config")
+gcp_connector_name = gcp_config.get('gcp_connector_name')        
+project_id = gcp_config.get('project-id')                
+zone_name = gcp_config.get('zone-name')                  
+disk_type = gcp_config.get('disk-type')                  
+image_project_id = gcp_config.get('image-project-id')     
+service_account_credenetials = gcp_config.get('service-account-credentials')
+
+ticket_id = input.get('freshdesk_webhook.ticket_id')
+ticket_id = ticket_id.replace(/^\D+/g, '')
+log.info("Ticket ID " + ticket_id)
+freshservice_config = input.get("freshservice_config")
+domain_name = freshservice_config.get('domain_name')
+email = freshservice_config.get('email')
+password = freshservice_config.get('password')
+status = freshservice_config.get('status')
+freshservice_connector_name = freshservice_config.get('freshservice_connector_name')
+ticket_type = freshservice_config.get('ticket_type')
+private_note = freshservice_config.get('private_note')
+// Ticket service item fields to be parsing
+ticket_service_item_fields = input.get('freshdesk_webhook.ticket_service_item_fields')
+log.trace("Ticket service field...:: " + ticket_service_item_fields)
+// Parse the service item fields by calling flintbit
+parse_flintbit_call_response = call.bit('flint-util:freshservice:extract_sr_items_fields.groovy')
+                                   .set('ticket_service_item_fields', ticket_service_item_fields)
+                                   .sync()
+// Get instance size(type) from the response of freshservice extract_sr_items_fields
+flint_action_name = parse_flintbit_call_response.get('data').get("Item Name")
+log.info("Flint action name....:: "+flint_action_name)
+instance_id = parse_flintbit_call_response.get('data').get('Instance ID')
+log.info("Instance id " + instance_id)
+
+group_name = parse_flintbit_call_response.get('data').get('Resource Group Name')
+log.info("Group name " + group_name)
+
+instance_name = parse_flintbit_call_response.get('data').get('Instance Name')          
+operating_system_type = parse_flintbit_call_response.get('data').get('Operating System Type')
+machine_type = parse_flintbit_call_response.get('data').get('Machine Type')
+log.info("Instance Name "+instance_name+" | Operating System Type "+operating_system_type+" | Machine Type "+machine_type)
+
+
+switch (flint_action_name) {
+    case "Start AWS EC2 Instance":
+        {
+            log.info("Start aws instance")
+            flintbit_response = call.bit("flint-util:freshservice:aws:aws_start_instance.js")
+                                    .set("aws_connector_name", aws_connector_name)
+                                    .set("security-key", security_key)
+                                    .set("access-key", access_key)
+                                    .set("instance-id", instance_id)
+                                    .set("region", aws_region)
+                                    .set('domain_name', domain_name)
+                                    .set('email', email)
+                                    .set('password', password)
+                                    .set('status', status)
+                                    .set('freshservice_connector_name', freshservice_connector_name)
+                                    .set('ticket_id', ticket_id)
+                                    .set('ticket_type', ticket_type)
+                                    .set('private_note',private_note)
+                                    .sync()
+        } break;
+        case "Create Azure Resource Group":
+        {
+            log.info("Create resource group")
+
+            log.info("Inputs .... azure_connector_name "+azure_connector_name+" | tenant_id "+tenant_id+" | subscription_id "+subscription_id+" | key "+key+" | client_id "+client_id+" | group_name "+group_name+" | azure_region "+azure_region)    
+            flintbit_response = call.bit("flint-util:freshservice:azure:azure_create_resource_group.js")
+                                    .set('azure_connector_name', azure_connector_name)
+                                    .set('tenant-id', tenant_id)
+                                    .set('subscription-id', subscription_id)
+                                    .set('key', key)
+                                    .set('client-id', client_id)
+                                    .set('group-name', group_name)
+                                    .set('region', azure_region)
+                                    .set('domain_name', domain_name)
+                                    .set('email', email)
+                                    .set('password', password)
+                                    .set('status', status)
+                                    .set('freshservice_connector_name', freshservice_connector_name)
+                                    .set('ticket_id', ticket_id)
+                                    .set('ticket_type', ticket_type)
+                                    .set('private_note',private_note)
+                                    .sync()
+        } break;
+        case "Delete Azure Resource Group":
+        {
+            log.info("Delete resource group")
+            log.info("Inputs .... azure_connector_name "+azure_connector_name+" | tenant_id "+tenant_id+" | subscription_id "+subscription_id+" | key "+key+" | client_id "+client_id+" | group_name "+group_name)    
+            flintbit_response = call.bit("flint-util:freshservice:azure:azure_delete_resource_group.js")
+                                    .set('azure_connector_name', azure_connector_name)
+                                    .set('tenant-id', tenant_id)
+                                    .set('subscription-id', subscription_id)
+                                    .set('key', key)
+                                    .set('client-id', client_id)
+                                    .set('resource-group-name', group_name)
+                                    .set('domain_name', domain_name)
+                                    .set('email', email)
+                                    .set('password', password)
+                                    .set('status', status)
+                                    .set('freshservice_connector_name', freshservice_connector_name)
+                                    .set('ticket_id', ticket_id)
+                                    .set('ticket_type', ticket_type)
+                                    .set('private_note',private_note)
+                                    .sync()
+        } break;
+        case "New GCP Instance":
+        {
+            log.info("Create gcp instance....")
+            log.info("gcp_connector_name "+gcp_connector_name+" | project_id "+project_id+" | zone_name "+zone_name+" | service_account_credenetials "+service_account_credenetials+" | instance_name "+instance_name+" | disk_type "+disk_type+" | operating_system_type "+operating_system_type+" | machine_type "+machine_type+" | image_project_id "+image_project_id)
+            flintbit_response = call.bit("flint-util:freshservice:gcp:gcp_create_instance.js")
+                                    .set('gcp_connector_name', gcp_connector_name)
+                                    .set('project-id', project_id)
+                                    .set('zone-name', zone_name)
+                                    .set('service-account-credentials', service_account_credenetials)
+                                    .set('instance-name', instance_name)
+                                    .set('disk-type', disk_type)
+                                    .set('image-name', operating_system_type)
+                                    .set('machine-type', machine_type)
+                                    .set('image-project-id', image_project_id)
+                                    .set('domain_name', domain_name)
+                                    .set('email', email)
+                                    .set('password', password)
+                                    .set('status', status)
+                                    .set('freshservice_connector_name', freshservice_connector_name)
+                                    .set('ticket_id', ticket_id)
+                                    .set('ticket_type', ticket_type)
+                                    .set('private_note',private_note)
+                                    .sync()
+        } break;
+        case "Delete GCP Instance":
+        {
+            log.info("delete gcp instance....")
+            flintbit_response = call.bit("flint-util:freshservice:gcp:gcp_delete_instance.js")
+                                    .set('gcp_connector_name', gcp_connector_name)
+                                    .set('project-id', project_id)
+                                    .set('zone-name', zone_name)
+                                    .set('service-account-credentials', service_account_credenetials)
+                                    .set('instance-name', instance_name)
+                                    .set('domain_name', domain_name)
+                                    .set('email', email)
+                                    .set('password', password)
+                                    .set('status', status)
+                                    .set('freshservice_connector_name', freshservice_connector_name)
+                                    .set('ticket_id', ticket_id)
+                                    .set('ticket_type', ticket_type)
+                                    .set('private_note',private_note)
+                                    .sync()
+        } break;
+
+
+}
