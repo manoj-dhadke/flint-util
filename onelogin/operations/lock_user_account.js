6802b053ca00c28f22e57fbeeefc0531819d41c3
akashhirve <akash.hirve@getflint.io>
2019-04-18 10:35:41 +0530

    Updated flintbit input checks and connector call

--- onelogin/operations/lock_user_account.js
+++ onelogin/operations/lock_user_account.js
@@ -6,7 +6,9 @@
 
 log.info("Started executing 'example:lock_user_account.js' flintbit")
 
+log.trace("Flintbit Inputs: \n"+input)
 
+action = 'lock-user-account'
 // Service parameters
 onelogin_config = input.get('onelogin_configurations')
 client_id = onelogin_config.get('client_id')
@@ -17,18 +19,27 @@ region = input.get('region')
 user_id = input.get('user_id')
 
 // Specified the duration in minutes, to lock the specified user account
-locked_until = input.get('locked_until')
+input_clone = JSON.parse(input)
 
-action = 'lock-user-account'
 
-connector_response = call.connector(connector_name)
+log.trace("Setting OneLogin Connector Inputs")
+
+response = call.connector(connector_name)
                         .set('client_id',client_id)
                         .set('client_secret', client_secret)
                         .set('region', region)
                         .set('action', action)
                         .set('user_id', user_id)
-                        .set('locked_until', locked_until)
-                        .sync()
+
+if(input_clone.hasOwnProperty('locked_until') && input_clone['locked_until'] != null || input_clone['locked_until'] != ""){
+    locked_until = input.get('locked_until')
+    response.set('locked_until', locked_until)
+
+}
+
+log.trace("Calling OneLogin Connector")
+// Call connector
+connector_response = response.sync()
 
 log.trace(connector_response)
 result = connector_response.get('status')
