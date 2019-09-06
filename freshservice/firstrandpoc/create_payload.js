3634edb24ed57d4363ced256f844bf8fdd346c81
akashhirve <akash.hirve@getflint.io>
2019-09-06 14:19:53 +0530

    Updated flintbit

--- freshservice/firstrandpoc/create_payload.js
+++ freshservice/firstrandpoc/create_payload.js
@@ -10,7 +10,25 @@
  payload = input.get("payload")
  log.trace("Payload is "+payload)
 
-payload = util.json(payload)
 log.trace("Payload converted to Json: "+payload)
 
-ouput.set("result", payload)
+// Make Http Post Request
+flintbit_response = call.bit('flint-util:http:operation:workflow:post.js')
+.set('url', url)
+.set('body', payload)
+.set('method', 'post')
+.sync()
+
+log.trace("POST flintbit reponse: "+flintbit_response)
+exit_code = flintbit_response.exitcode()
+message = flintbit_response.message()
+
+if(exit_code == 0){
+    log.trace("Exit code is "+exit_code)
+    output.set(0, message).set('result', flintbit_response.get('result'))
+}else{
+    log.trace("POST request failed due to "+message)
+    output.set(-1, message).set('error', message)
+}
+
+ouput.set("result", payload).set("exit-code", 0)
