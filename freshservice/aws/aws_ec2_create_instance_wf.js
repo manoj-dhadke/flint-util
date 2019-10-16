4389fc263d103d286c1510e3d03dfe65b3560c38
atharva-infiverve <atharva.deshmukh@getflint.io>
2019-10-16 16:27:07 +0530

    updated create aws wrapper flintbit

--- freshservice/aws/aws_ec2_create_instance_wf.js
+++ freshservice/aws/aws_ec2_create_instance_wf.js
@@ -99,12 +99,11 @@ log.info(create_aws_flintbit_call_response)
 
 
 if(create_instance_exit_code == 0){
-    log.trace("Instance created successfully with: \nInstance ID:"+instance_id+"\nPrivate IP: "+private_ip)
-    // Getting instance information
     instance_info = create_aws_flintbit_call_response.get('instances-info')
     instance_id = instance_info[0].get('instance-id')
     log.info("Instance_Info: " + instance_id)
-    // Getting private IP
+    log.trace("Instance created successfully with: \nInstance ID:"+instance_id+"\nPrivate IP: "+private_ip)
+    
     private_ip = instance_info[0].get('private-ip')
     log.info("Private IP: " + private_ip)
     output.set('instance_details', instance_info[0])
