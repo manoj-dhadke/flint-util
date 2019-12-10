c396f7c802cac6c02e849a00cffdc8dce8ad0d35
Manoj Dhadke <manoj.dhadke@getflint.io>
2019-12-10 15:57:37 +0530

    Flint license header changed

--- azure_ad/operations/add_user_to_group.js
+++ azure_ad/operations/add_user_to_group.js
@@ -1,19 +1,19 @@
-/*
- *
+/*************************************************************************
+ * 
  * INFIVERVE TECHNOLOGIES PTE LIMITED CONFIDENTIAL
- * _______________________________________________
- *
- *  (C) INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE
- *  All Rights Reserved.
- *  Product / Project: Flint IT Automation Platform
- *  NOTICE:  All information contained herein is, and remains
- *  the property of INFIVERVE TECHNOLOGIES PTE LIMITED.
- *  The intellectual and technical concepts contained
- *  herein are proprietary to INFIVERVE TECHNOLOGIES PTE LIMITED.
- *  Dissemination of this information or any form of reproduction of this material
- *  is strictly forbidden unless prior written permission is obtained
- *  from INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE.
-*/
+ * __________________
+ * 
+ * (C) INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE
+ * All Rights Reserved.
+ * Product / Project: Flint IT Automation Platform
+ * NOTICE:  All information contained herein is, and remains
+ * the property of INFIVERVE TECHNOLOGIES PTE LIMITED.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to INFIVERVE TECHNOLOGIES PTE LIMITED.
+ * Dissemination of this information or any form of reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE.
+ */
 
 log.trace("Started executing 'fb-cloud:azure:operation:add_user_to_group.js' flintbit")
 
